f88364748a1432f02e62ab7e4ad108a01794f360
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-26 13:16:20 -0500

    hid all divs except about me section and converted navbar events to jquery

--- javascripts/components/projects.js
+++ javascripts/components/projects.js
@@ -10,17 +10,18 @@ const showProjects = () => {
   return projects;
 };
 
-const projectAvailability = e => {
-  const available = e.target.id;
-  if (available === true) {
-    projectsBuilder(projects);
-  } else if (available === false) {
-    const filteredProjects = projects.filter(x => x.available === available);
-    projectsBuilder(filteredProjects);
-  } else {
-    projectsBuilder(projects);
-  }
-};
+// const projectAvailability = e => {
+//   const available = e.target.id;
+//   if (available === true) {
+//     projectsBuilder(projects);
+//   } else if (available === false) {
+//     const filteredProjects = projects.filter(x => x.available === available);
+//     projectsBuilder(filteredProjects);
+//   } else {
+//     projectsBuilder(projects);
+//   }
+// };
+
 const projectsBuilder = projectsArray => {
   let domString = "";
   projectsArray.forEach(project => {
@@ -44,52 +45,18 @@ const projectsBuilder = projectsArray => {
   printToDom(domString);
 };
 
-// const projectsBuilder = () => {
-//   let newString = "";
-//   for (let i = 0; i < projects.length; i++) {
-//     newString += `<div id="projectsPage">`;
-//     newString += `<div id="projects-card">`;
-//     newString += `<h6>${projects[i].title}</h6>`;
-//     newString += `<a href="https://${
-//       projects.screenshot
-//     }" target="_blank">View Image</a>`;
-//     newString += `<h6>${projects[i].description}</h6>`;
-//     newString += `<h6>${projects[i].technologiesUsed}</h6>`;
-//     newString += `<h6>${projects[i].available}</h6>`;
-//     newString += `<a href="https://${
-//       projects.url
-//     }" target="_blank">View Project Here</a>`;
-//     newString += `<a href="https://${
-//       projects.githubUrl
-//     }" target="_blank">View Github Here</a>`;
-//     newString += "</div>";
-//     newString += "</div>";
-//     printToDom(newString, "projectsPage");
-//   }
-// };
-
-function divDisplay(divId, hide1, hide2) {
-  document.getElementById(divId).style.display = "block";
-  document.getElementById(hide1).style.display = "none";
-  document.getElementById(hide2).style.display = "none";
-}
-
-const bioNav = document
-  .getElementById("navToBio")
-  .addEventListener("click", function() {
-    divDisplay("bioPage", "technologyPage", "projectsPage");
+$(document).ready(function() {
+  $("#technologyPage").hide();
+  $("#navToTechnologies").on("click", function() {
+    $("#technologyPage").toggle();
   });
-
-const techNav = document
-  .getElementById("navToTechnologies")
-  .addEventListener("click", function() {
-    divDisplay("technologyPage", "bioPage", "projectsPage");
+  $("#projectsPage").hide();
+  $("#navToProjects").on("click", function() {
+    $("#projectsPage").toggle();
   });
-
-const projectNav = document
-  .getElementById("navToProjects")
-  .addEventListener("click", function() {
-    divDisplay("projectsPage", "technologyPage", "bioPage");
+  $("#navToBio").on("click", function() {
+    $("#bioPage").toggle();
   });
+});
 
-export { projectsBuilder, showProjects, setProjects, projectAvailability };
+export { projectsBuilder, showProjects, setProjects };
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,8 @@
 import { getProjects } from "./data/projectData.js";
-import { projectAvailability } from "./components/projects.js";
+// import { projectAvailability } from "./components/projects.js";
 
 const initializeApp = () => {
   getProjects();
-  projectAvailability();
 };
 
 initializeApp();
