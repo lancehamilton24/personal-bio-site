95b75d82637191ef6bfafe3b2573027388bc73df
Lance Hamilton <lance.hamilton24@gmail.com>
2018-11-04 22:19:18 -0600

    added webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import $ from 'jquery';
 import 'bootstrap';
-
 import './index.scss';
+import axios from 'axios';
+
+const projectsBuilder = (projects) => {
+  let newString = '';
+  projects.forEach((project) => {
+    newString += `<div class='col-sm-4  pet-card'>;
+     <div class='card border-success'>;
+    <div class='card-header bg-transparent border-success'>${project.title};
+    <div class='card-body text-success'>;
+    <h5 class='card-title'>${project.color}</h5>;
+    <p class='card-text petSkill'>${project.description}</p>;
+    <div class='card-footer bg-transparent border-success petType'>${project.available};
+    </div>;
+    </div>;
+    </div>;
+    </div>;
+    </div>`;
+  });
+  $('#projectsPage').append(newString);
+};
+
+const getProjects = () => axios.get('../db/projects.json');
+
+getProjects()
+  .then((data) => {
+    projectsBuilder(data.projects);
+  })
+  .catch((error) => {
+    console.error({ error });
+  });
 
 
-$('#click-button').on('click', () => {
-  $('#important-text').append('<p>Hello World!</p>');
-});
+$('#projectsPage').show();
