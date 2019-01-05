0b90a69afa6b5127718e0485dec2d63e9d3ee083
Lance Hamilton <lance.hamilton24@gmail.com>
2019-01-05 16:30:45 -0600

    created technology cards

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import './aboutMe.scss';
+import bioPhoto from '../../images/bioPhoto.png';
 
 
 const introBuilder = () => {
@@ -12,9 +13,21 @@ const introBuilder = () => {
   $('#intro').html(domString);
 };
 
+const bioBuilder = () => {
+  let domString = '';
+  domString += `<div>
+   <img class='card-img-top' src="${bioPhoto}" alt="linkedin">
+   <h1>About Me</h1>
+   <hr>
+   <h3>Software Developer</h3>
+   </div>`;
+  $('#bioPage').html(domString);
+};
+
 
 const initializeIntroPage = () => {
   introBuilder();
+  bioBuilder();
 };
 
 export default initializeIntroPage;
