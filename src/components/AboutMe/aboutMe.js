4d4e20a2a004a95fbe9e8d0f7c9d28f4de1626b8
Lance Hamilton <lance.hamilton24@gmail.com>
2019-01-07 16:14:12 -0600

    completed aboutMe

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import './aboutMe.scss';
-import bioPhoto from '../../images/bioPhoto1.png';
+import bioPhoto from '../../images/bioImage2.png';
 
 
 const introBuilder = () => {
@@ -22,8 +22,12 @@ const bioBuilder = () => {
   <hr>
   <div id='bioImage'>
   <img class='card-img-bio' src="${bioPhoto}" alt="linkedin">
-  <p>Nasvhille Native djkl;ajsdfkjasdlk;jfklasdjfklsd;a
-     dkasjfkldsajfkldsjafklasdjfkljsdklfjdslkjfksdljflks</p>
+  <div id='bioText'>
+  <p>Nashville native and University of Rochester graduate with a love for technology and programming. Growing up with a passion for building and fixing computers, and using my free time to resolve hardware issues, has led me to pursue a career in software development.<br><br>
+  After graduating from Rochester in 2016 with a Bachelor's degree in Intellectual and Cultural History, I worked in a variety of capacities—as an IT Consultant, baseball coach, and Jimmy John’s deliverer driver. During this time I began to explore different career options, focusing primarily on technology-related fields. The multitude of free web resources online sparked my fascination with programming.<br><br>
+  After taking several introductory classes and practicing on my own, I decided to delve deeper by applying to Nashville Software School to further my education. I am excited to use the software development skills I have acquired and continue to strengthen to solve complex problems and simplify daily processes for individuals and companies. 
+  </p>
+  </div>
   </div>
    </div>`;
   $('#bioPage').html(domString);
