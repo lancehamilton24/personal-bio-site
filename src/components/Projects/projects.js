219d3abdbd1c66b3315fc7af35cca77e02fe3b2c
Lance Hamilton <lance.hamilton24@gmail.com>
2019-01-08 00:35:49 -0600

    .

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -10,7 +10,7 @@ const projectsBuilder = (projectsArray) => {
     projectString += `
     <div class ="projectsCard">
     <div class="card ${project.available}">
-            <img class="card-img" src="${project.screenshot}" alt="">
+            <img class="card-img projectImage" src="${project.screenshot}" alt="">
             <h6>${project.title}</h6>
             <a href="https://${project.url}" target="_blank">View Project Here</a>
             <a href="https://${project.githubUrl}" target="_blank">View Github Here</a>
